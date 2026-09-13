3446f1abfd39826b13ab456f4199b4d4967c38dc
bezawit525 <bezahayalu75@gmail.com>
2026-09-14 00:18:09 +0300

    create club card

---
+++ module 3/Day 32/Campus Connect/src/components/ClubCard/ClubCard.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { useFavorites } from "../../Context/FavoritesContext";
+
+function ClubCard({ club }) {
+  const { favorites, toggleFavorite } = useFavorites();
+
+  const isFavorite = favorites.includes(club.id);
+
+  return (
+    <article className="card">
+      <img src={club.image} alt={club.name} />
+
+      <div className="card-content">
+        <span className="category">{club.category}</span>
+
+        <h3>{club.name}</h3>
+
+        <p>{club.description}</p>
+
+        <div className="card-actions">
+          <Link to={`/clubs/${club.id}`} className="button">
+            View Details
+          </Link>
+
+          <button
+            className="favorite-button"
+            onClick={() => toggleFavorite(club.id)}
+          >
+            {isFavorite ? "★ Favorite" : "☆ Favorite"}
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default ClubCard;
