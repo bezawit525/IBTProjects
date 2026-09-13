23d4e6228f17cb4b97399075ff24221eb5b5f772
bezawit525 <bezahayalu75@gmail.com>
2026-09-14 00:04:26 +0300

    create the Navbar

---
+++ module 3/Day 32/Campus Connect/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <NavLink to="/" className="logo">
+          Campus Connect
+        </NavLink>
+        <div className="nav-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/clubs">Clubs</NavLink>
+          <NavLink to="/events">Events</NavLink>
+          <NavLink to="/resources">Resources</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
